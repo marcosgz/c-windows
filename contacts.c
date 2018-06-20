6cdc7ff0b4902014ed0cd6a92b6ec740b5f114e6
Marcos G. Zimmermann <mgzmaster@gmail.com>
2018-06-20 20:20:01 -0300

    Add structure to the removeContact and showContact

--- contacts.c
+++ contacts.c
@@ -1,49 +1,52 @@
 #include <stdio.h>
 #include "contacts.h"
 
-#define OPTIONS_LENGHT 6
-
 int userMenuOption() {
   int option = -1;
   printf("\n> ");
   scanf("%d", &option);
 
-  if (option > 6 || option < 1) {
+  if (option > 7 || option < 1) {
     printf("Opcao nao e valida. Precisa ser um numero de 1 a 6\n");
   }
 
   return option;
 }
 
+Contact addContact() {
+  Contact contact;
+  return contact;
+}
+
+void removeContact(Contact* contact) {
+
+}
+
+void showContact(int id) {
+
+}
+
 // Print a simple menu with all options available to the user
 void showMenu() {
-  char options[OPTIONS_LENGHT][50];
-  sprintf(options[0], "[1] Cadastrar Contato");
-  sprintf(options[1], "[2] Listar contatos");
-  sprintf(options[2], "[3] Procurar contato");
-  sprintf(options[3], "[4] Eliminar contato");
-  sprintf(options[4], "[5] Eliminar banco de dados");
-  sprintf(options[5], "[6] Sair do sistema");
+  int len = 7;
+  char options[len][50];
+  sprintf(options[0], "[1] Cadastrar novo contato");
+  sprintf(options[1], "[2] Exibir contato");
+  sprintf(options[2], "[3] Listar todos contatos");
+  sprintf(options[3], "[4] Procurar contato");
+  sprintf(options[4], "[5] Apagar contato");
+  sprintf(options[5], "[6] Eliminar banco de dados");
+  sprintf(options[6], "[%d] Sair do sistema", EXIT_CODE);
 
   int i;
-  for(i=0; i < OPTIONS_LENGHT; i ++) {
+  for(i=0; i < len; i ++) {
     printf("%s\n", options[i]);
   }
 }
-
-// Global Variables
-typedef struct contact {
-  char name[50];
-  char address[100];
-  char phoneArea[3];
-  char phone[13];
-  char email[30];
-  char details[255];
-}Contact;
 
 // Main Function
 int main(void) {
   do {
     showMenu();
-  } while(userMenuOption() != 6);
+  } while(userMenuOption() != EXIT_CODE);
 }
--- contacts.h
+++ contacts.h
@@ -1,3 +1,17 @@
+// Global Variables
+#define EXIT_CODE 7
+int latestContactID = 0; // Using this variable as our Contact AUTO_INCREMENT ID
+
+typedef struct contact {
+  int id;
+  char name[50];
+  char address[100];
+  char phoneArea[3];
+  char phone[13];
+  char email[30];
+  char details[255];
+}Contact;
+
 // Functions Prototypes
 int userMenuOption();
 void showMenu();
